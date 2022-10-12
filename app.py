1ee1d043f2713179a059f4858bac4033be2a14d7
Panisara Sonkom <[psonkom342@gmail.com]>
2022-10-13 10:53:04 +1100

    added cards

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask  import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
-db= SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -14,15 +14,16 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text)
-    date = db.Column(db.String)
+    date = db.Column(db.Date)
     status = db.Column(db.String)
     priority = db.Column(db.String)
 
 
+# Define a custom CLI (terminal) command
 @app.cli.command('create')
 def create_db():
     db.create_all()
-    print("Table created")
+    print("Tables created")
 
 @app.cli.command('drop')
 def drop_db():
@@ -31,17 +32,41 @@ def drop_db():
 
 @app.cli.command('seed')
 def seed_db():
-    card = Card(
-        title = 'Start the project',
-        description = 'Stage 1 - Creating the database',
-        status = 'To do',
-        priority = 'High',
-        date = date.today()
-    )
+    cards = [
+        Card(
+            title = 'Start the project',
+            description = 'Stage 1 - Create the database',
+            status = 'To Do',
+            priority = 'High',
+            date = date.today()
+        ),
+        Card(
+            title = "SQLAlchemy",
+            description = "Stage 2 - Integrate ORM",
+            status = "Ongoing",
+            priority = "High",
+            date = date.today()
+        ),
+        Card(
+            title = "ORM Queries",
+            description = "Stage 3 - Implement several queries",
+            status = "Ongoing",
+            priority = "Medium",
+            date = date.today()
+        ),
+        Card(
+            title = "Marshmallow",
+            description = "Stage 4 - Implement Marshmallow to jsonify models",
+            status = "Ongoing",
+            priority = "Medium",
+            date = date.today()
+        )
+    ]
 
-    db.session.add(card)
+    db.session.add_all(cards)
     db.session.commit()
-    print('Table seed')
+    print('Tables seeded')
+
 
 @app.route('/')
 def index():
