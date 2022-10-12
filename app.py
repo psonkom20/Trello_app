2888bfd37745f103151ede9a355d501bf1d1d7e7
Panisara Sonkom <[psonkom342@gmail.com]>
2022-10-12 12:43:09 +1100

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask  import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
+
+db= SQLAlchemy(app)
+
+print(db.__dict__)
+@app.route('/')
+def index():
+    return "Hello World!"
