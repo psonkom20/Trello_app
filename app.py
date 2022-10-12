e380a2a50a021524e8e7f640f7dee730d01013ef
Panisara Sonkom <[psonkom342@gmail.com]>
2022-10-12 15:23:31 +1100

    added sqlalchemy and cli commands

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask  import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
@@ -7,7 +8,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:passwo
 
 db= SQLAlchemy(app)
 
-print(db.__dict__)
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    date = db.Column(db.String)
+    status = db.Column(db.String)
+    priority = db.Column(db.String)
+
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print("Table created")
+
+@app.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print("Tables dropped")
+
+@app.cli.command('seed')
+def seed_db():
+    card = Card(
+        title = 'Start the project',
+        description = 'Stage 1 - Creating the database',
+        status = 'To do',
+        priority = 'High',
+        date = date.today()
+    )
+
+    db.session.add(card)
+    db.session.commit()
+    print('Table seed')
+
 @app.route('/')
 def index():
     return "Hello World!"
