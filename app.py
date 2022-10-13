9f51e7c0c373766ac5b712e329182b8eafa018ac
Panisara Sonkom <[psonkom342@gmail.com]>
2022-10-13 14:40:31 +1100

    added queries and marshmallow

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config ['JSON_SORT_KEYS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -17,6 +20,11 @@ class Card(db.Model):
     date = db.Column(db.Date)
     status = db.Column(db.String)
     priority = db.Column(db.String)
+
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description', 'status', 'priority', 'date')
+        ordered = True
 
 
 # Define a custom CLI (terminal) command
@@ -66,6 +74,34 @@ def seed_db():
     db.session.add_all(cards)
     db.session.commit()
     print('Tables seeded')
+
+
+@app.route('/cards/')
+def all_cards():
+    # select * from cards;
+    # cards = Card.query.all()
+    # stmt = db.select(Card).where(Card.status == 'To Do')
+    stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
+    cards = db.session.scalars(stmt)
+    return CardSchema(many=True).dump(cards)
+    # for card in cards:
+    #     print(card.title, card.priority)
+    # print(cards)
+    # print(cards[0].__dict__)
+
+@app.cli.command('first_card')
+def first_card():
+    # select * from cards limit 1;
+    # card = Card.query.first()
+    stmt = db.select(Card).limit(1)
+    card = db.session.scalar(stmt)
+    print(card.__dict__)
+
+@app.cli.command('count_ongoing')
+def count_ongoing():
+    stmt = db.select(db.func.count()).select_from(Card).filter_by(status='Ongoing')
+    cards = db.session.scalar(stmt)
+    print(cards)
 
 
 @app.route('/')
