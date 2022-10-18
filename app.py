ae6e1dd1daf53a749deeb9639375c9796f35bf6e
Panisara Sonkom <[psonkom342@gmail.com]>
2022-10-18 15:12:08 +1100

    /auth/register implemented

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config ['JSON_SORT_KEYS'] = False
@@ -10,6 +12,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:passwo
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password', 'is_admin')
+
+
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -40,6 +58,19 @@ def drop_db():
 
 @app.cli.command('seed')
 def seed_db():
+    users = [
+        User(
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('eggs').decode('utf-8'),
+            is_admin=True
+        ),
+        User(
+            name='John Cleese',
+            email='someone@spam.com',
+            password=bcrypt.generate_password_hash('12345').decode('utf-8')
+        )
+    ]
+
     cards = [
         Card(
             title = 'Start the project',
@@ -72,9 +103,29 @@ def seed_db():
     ]
 
     db.session.add_all(cards)
+    db.session.add_all(users)
     db.session.commit()
     print('Tables seeded')
 
+@app.route('/auth/register/', methods=['POST'])
+def auth_register():
+    try:
+        #load the posted user info and parse the JSON
+        user_info = UserSchema().load(request.json)
+        #Create a new User model instance from the user_info
+        user = User(
+        email = user_info['email'],
+        password = bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+        name = user_info['name']
+        )
+        db.session.add(user)
+        #Add and commint user to DB
+        db.session.add(user)
+        db.session.commit()
+        #Respond to client
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
 
 @app.route('/cards/')
 def all_cards():
